6deb32ad68d5f6c5333b9f53a8818b67f7b33e56
Andrew Redgwell <andrew@amr.nz>
2023-04-11 14:24:46 +1200

    build: Add formula

---
+++ Formula/kinetik-cli.rb
@@ -0,0 +1,31 @@
+class KinetikCli < Formula
+  desc "Brew Formula for Kinetik Command-line Interface (CLI)"
+  homepage "https://github.com/section6nz/homebrew-kinetik-base"
+  version "0.13.3"
+  license "Apache-2.0"
+  head "https://github.com/section6nz/homebrew-kinetik-base", branch: "main"
+  if Hardware::CPU.arm?
+    url "https://github.com/section6nz/kinetik-base/releases/download/v#{version}/kinetik-main-aarch64-apple-darwin"
+    sha256 "8a7a2297d9700747c4d88e2eea9ad349c92ed89aa1c65de27ab088e8d1718650"
+  else
+    url "https://github.com/section6nz/kinetik-base/releases/download/v#{version}/kinetik-main-x86_64-apple-darwin"
+    sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
+  end
+  bottle do
+    root_url "https://ghcr.io/v2/aredgwell/kinetik-cli"
+  end
+
+  def install
+    if Hardware::CPU.arm?
+      bin.install "#{version}/bin/kinetik-main-aarch64-apple-darwin" => "kinetik"
+    else
+      bin.install "#{version}/bin/kinetik-main-x86_64-apple-darwin" => "kinetik"
+    end
+    chmod 0755, "#{bin}/kinetik"
+    system "xattr", "-dr", "com.apple.quarantine", "#{bin}/kinetik"
+  end
+
+  test do
+    system "#{bin}/kinetik", "--version"
+  end
+end
